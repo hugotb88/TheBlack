b8ef17efbe025457b36bb89aac4798cb427ba7c2
Victor Hugo Olvera Cruz <victor olvera@tiempodev.local>
2018-10-04 13:01:15 -0500

    First upload

---
+++ src/main/java/com/theblack/The/Black/TheBlackApplication.java
@@ -0,0 +1,12 @@
+package com.theblack.The.Black;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBlackApplication.class, args);
+	}
+}
