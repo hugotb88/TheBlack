4f238f6d76b3645b5abbc471cec9451abcfc3cf0
Victor Hugo Olvera Cruz <victor olvera@tiempodev.local>
2018-10-10 11:54:12 -0500

    Commit just for commit

--- src/main/java/com/theblack/The/Black/controllerFeign/UserFeignController.java
+++ src/main/java/com/theblack/The/Black/controllerFeign/UserFeignController.java
@@ -1,8 +1,13 @@
 package com.theblack.The.Black.controllerFeign;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theblack.The.Black.model.User;
 import com.theblack.The.Black.serviceFeign.UserServiceFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,35 +18,44 @@ import java.util.ArrayList;
  *
  */
 @Controller
-//@ExceptionHandler("className".class) //How to manage Exceptions
 public class UserFeignController {
 
     @Autowired
     UserServiceFeign userServiceFeign;
 
     //Create new UserService
-    @RequestMapping(value="/feign/user", method = RequestMethod.POST)
+    @RequestMapping(
+            value="/feign/user",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public User createUser(@RequestBody User user){
-        return userServiceFeign.createUser(user);
+    public User createUser(@RequestBody User user) throws Exception {
+        User result = userServiceFeign.createUser(user);
+        return result;
     }
 
     //Get All the Users
-    @RequestMapping(value="/feign/user", method = RequestMethod.GET)
+    @RequestMapping(
+            value="/feign/user",
+            method = RequestMethod.GET)
     @ResponseBody
     public ArrayList<User> getAllUsers(){
         return userServiceFeign.getAllUsers();
     }
 
     //Get an user by Mail
-    @RequestMapping(value="/feign/user/{mail}", method = RequestMethod.GET)
+    @RequestMapping(
+            value="/feign/user/{mail}",
+            method = RequestMethod.GET)
     @ResponseBody
     public User getUserByMail(@PathVariable(value="mail") String mail){
         return userServiceFeign.getUserByMail(mail);
     }
 
     //Delete an User
-    @RequestMapping(value = "/feign/user/{idUser}", method = RequestMethod.DELETE)
+    @RequestMapping(
+            value = "/feign/user/{idUser}",
+            method = RequestMethod.DELETE)
     @ResponseBody
     public String deleteUser(@PathVariable Long idUser){
         if(userServiceFeign.deleteUser(idUser) == 0){
@@ -52,7 +66,9 @@ public class UserFeignController {
     }
 
     //Update an User
-    @RequestMapping(value = "/feign/user", method = RequestMethod.PUT)
+    @RequestMapping(
+            value = "/feign/user",
+            method = RequestMethod.PUT)
     @ResponseBody
     public User updateUser (@RequestBody User user){
         return userServiceFeign.updateUser(user);
